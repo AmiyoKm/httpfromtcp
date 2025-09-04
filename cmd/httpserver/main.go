45feba71fb495611d45fe8bcfc88c479ee35c015
AmiyoKm <games.amiyo@gmail.com>
2025-09-04 22:26:30 +0600

    feat: enhance response handling in server with streaming support and error management

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/AmiyoKm/httpfromtcp/internal/request"
@@ -17,26 +20,58 @@ const port = 42069
 var handler = server.Handler(func(w *response.Writer, r *request.Request) {
 	headers := response.GetDefaultHeaders(0)
 	headers.Replace("Content-Type", "text/html")
+	if strings.HasPrefix(r.RequestLine.RequestTarget, "/httpbin/stream") {
+		target := r.RequestLine.RequestTarget
+		res, err := http.Get("https://httpbin.org/" + target[len("/httpbin/"):])
+		if err != nil {
+			body := respond500()
+
+			w.WriteStatusLine(response.StatusInternalServerError)
+			headers.Replace("Content-Length", strconv.Itoa(len(body)))
+			w.WriteHeaders(*headers)
+			w.WriteBody(body)
+			return
+		}
+		w.WriteStatusLine(response.StatusOK)
+
+		headers.Delete("Content-Length")
+		headers.Set("transfer-encoding", "chunked")
+		headers.Replace("Content-Type", "text/plain")
+		w.WriteHeaders(*headers)
+
+		for {
+			data := make([]byte, 32)
+			n, err := res.Body.Read(data)
+			if err != nil {
+				break
+			}
+			w.WriteBody([]byte(fmt.Sprintf("%x\r\n", n)))
+			w.WriteBody(data[:n])
+			w.WriteBody([]byte("\r\n"))
+		}
+		w.WriteBody([]byte("0\r\n\r\n"))
+		return
+	}
 	switch r.RequestLine.RequestTarget {
 	case "/yourproblem":
 		body := respond400()
 		w.WriteStatusLine(response.StatusBadRequest)
 		headers.Replace("Content-Length", strconv.Itoa(len(body)))
 		w.WriteHeaders(*headers)
-		w.WriteBody(respond400())
+		w.WriteBody(body)
 	case "/myproblem":
 		body := respond500()
 
 		w.WriteStatusLine(response.StatusInternalServerError)
 		headers.Replace("Content-Length", strconv.Itoa(len(body)))
 		w.WriteHeaders(*headers)
-		w.WriteBody(respond500())
+		w.WriteBody(body)
 	default:
 		body := respond200()
 		w.WriteStatusLine(response.StatusOK)
 		headers.Replace("Content-Length", strconv.Itoa(len(body)))
 		w.WriteHeaders(*headers)
-		w.WriteBody(respond200())
+		w.WriteBody(body)
 	}
 })
 
